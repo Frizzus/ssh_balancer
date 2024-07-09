30b293a3b5485c5f22affd93572b277c04ae08de
Loïc Canin <loic.canin@gmail.com>
2024-07-09 10:50:32 +0200

    added a container/custom directory where you have a script lauchned at image creation and a config folder for the sudoers

---
+++ container/custom/root_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# This is a file that will be executed on building the image
+# you can :
+# - install softwares with apt
+# - create other permanent users
+# and so on
+
+apt install -y nano
+
