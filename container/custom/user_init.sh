4a37cd5880c2ef19f46dc9d745972862143734ae
Loïc Canin <loic.canin@gmail.com>
2024-07-11 11:24:54 +0200

    new file organisation for customizing the project by developper

---
+++ container/custom/user_init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# from this file you can :
+# -create new user, files, permission ...
+# everything int the /home/{user} directory will be deleted upon disconnection.
+# You have access to some variables
+# - $USERNAME : the base username of the temporary user
+# - $username : the complete username of the temporary user
+# - $user_pass : the password of the temporary user (randomly generated)
